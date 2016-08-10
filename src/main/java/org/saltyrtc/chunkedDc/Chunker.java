a4cf837c5a1c18ff1c6c94adf533ce029eaa710c
Danilo Bargen <danilo.bargen@threema.ch>
2016-08-10 18:16:05 +0200

    Started work on Chunker

--- src/main/java/org/saltyrtc/chunkedDc/Chunker.java
+++ src/main/java/org/saltyrtc/chunkedDc/Chunker.java
@@ -10,15 +10,79 @@ package org.saltyrtc.chunkedDc;
 
 import org.slf4j.Logger;
 
+import java.nio.ByteBuffer;
+
+/**
+ * A chunker instance splits up a ByteBuffer into multiple chunks.
+ *
+ * A header is added to each chunk:
+ *
+ * |C|IIII|SSSS|
+ *
+ * - C: Configuration bitfield (1 byte)
+ * - I: Id (4 bytes)
+ * - S: Serial number (4 bytes)
+ *
+ * The configuration bitfield looks as follows:
+ *
+ * |000000E|
+ *        ^---- End-of-message
+ */
 public class Chunker {
 
     // Logger
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger("ChunkedDC");
 
+    private final long id;
+    private final ByteBuffer buf;
+    private final int chunkSize;
+    private int chunkId;
+
+    @SuppressWarnings("FieldCanBeLocal")
+    public static int HEADER_LENGTH = 9;
+
     /**
      * Create a Chunker instance.
+     *
+     * @param id An identifier for the chunk. Must be betwen 0 and 2**32-1.
+     * @param buf The ByteBuffer containing the data that should be chunked.
+     * @param chunkSize The chunk size *excluding* header data.
      */
-    public Chunker() {
+    public Chunker(long id, ByteBuffer buf, int chunkSize) {
+        this.id = id;
+        this.buf = buf;
+        this.chunkSize = chunkSize;
+        this.chunkId = 0;
+    }
+
+    /**
+     * Return the next chunk.
+     */
+    public ByteBuffer next() {
+        // Allocate chunk buffer
+        final int remaining = this.buf.remaining();
+        final int chunkBytes = remaining < this.chunkSize ? remaining : this.chunkSize;
+        final ByteBuffer chunk = ByteBuffer.allocate(chunkBytes + HEADER_LENGTH);
+
+        // Create header
+        final byte config = remaining > chunkBytes ? (byte) 1 : (byte) 0;
+        final int id = UnsignedHelper.getUnsignedInt(this.id);
+        final int serial = UnsignedHelper.getUnsignedInt(this.nextSerial());
+
+        chunk.put(config);
+        chunk.putInt(id);
+        chunk.putInt(serial);
+        for (int i = 0; i < chunkBytes; i++) {
+            chunk.put(this.buf.get());
+        }
+        return (ByteBuffer) chunk.flip();
+    }
+
+    /**
+     * Return the id of the next block
+     */
+    private int nextSerial() {
+        return this.chunkId++;
     }
 
 }
